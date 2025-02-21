14acb6a75d65671b79325f6a4b4a06e102ab7b76
unknown <ictgiants@gmail.com>
2025-02-21 06:12:12 +0300

    home 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Thank you David-updated at home</ThemedText>
+        <ThemedText type="title">Thank you David-updated at home 2222</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
